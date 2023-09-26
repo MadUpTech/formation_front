2de4ec7fb7c8d06787eb94e158275d8eb4edfc9d
MadUpTech <145967137+MadUpTech@users.noreply.github.com>
2023-09-26 08:48:51 +0300

    Delete src directory

--- src/app/model/inscription.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Utilisateur {
-  id: number;
-  nom: string;
-  prenom: string;
-  adresse: string;
-  email: string;
-  password: string
-  telephone: string
-}
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  backEndHost: "http://localhost:8081"
-
-};
